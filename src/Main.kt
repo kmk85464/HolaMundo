f3fb950e36d0c48a5ee3c48b2a6ba7bcd090f19a
Roberto Calvo <KMK85464@educastur.es>
2024-10-23 21:11:47 +0200

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(){
+
+    /* programa inicial kotlin */
+    val saludo:String;
+    val sc = Scanner(System.`in`);
+    println("Dame saludo");
+    saludo = sc.next();
+    println("mi saludo ha sido " + saludo);
+
+}
